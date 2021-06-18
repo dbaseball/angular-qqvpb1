65f84b0214b60ed5bea367ff8fc2d5ebb13ddf62
dbaseball <rjmcgowan101@gmail.com>
2021-06-18 13:08:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qqvpb1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qqvpb1)
---
+++ src/app/mysvg/mysvg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mysvg',
+  templateUrl: './mysvg.component.html',
+  styleUrls: ['./mysvg.component.css']
+})
+export class MysvgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
